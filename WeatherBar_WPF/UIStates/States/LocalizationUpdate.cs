455caebdb5f66607156ac959e94d2feb8d1c509d
BISCUITIC <tort24457@gamil.com>
2025-08-25 17:13:22 +0300

    Добавил класс обработки локализации для UI

---
+++ WeatherBar_WPF/UIStates/States/LocalizationUpdate.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using Localization;
+using System.Windows;
+using WeatherBar_WPF.CustomUI;
+using WeatherIconGenerator;
+
+namespace WeatherBar_WPF.UIStates.States;
+
+internal class LocalizationUpdate: UIState
+{
+    private readonly ILocalizationData _localization;
+
+    public LocalizationUpdate(ILocalizationData localization)
+    {
+        _localization = localization;
+    }
+
+    public override void Apply(MainPanel ui)
+    {                
+        ui.WeatherDataPanel.Temperature.Prefix = $"{_localization.Temperature} : ";
+        ui.WeatherDataPanel.Pressure.Prefix = $"{_localization.Pressure} : ";
+        ui.WeatherDataPanel.Humidity.Prefix = $"{_localization.Humidity} : ";
+        ui.WeatherDataPanel.WindSpeed.Prefix = $"{_localization.WindSpeed} : ";
+        ui.WeatherDataPanel.Description.Prefix = $"{_localization.Description} : ";
+        
+        ui.BottomPanel.ExitButton.Content = _localization.Exit;
+        ui.BottomPanel.SettingsButton.Content = (_localization as LanguageLocalization)?.Language;
+    }
+}
